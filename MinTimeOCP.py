4d350ae60c68ca0222c19e7955193d84c06248bd
arog <arog3632@vt.edu>
2016-10-14 17:19:39 -0400

    Added many comments

--- MinTimeOCP.py
+++ MinTimeOCP.py
@@ -4,25 +4,64 @@ import cvxpy as cvx
 import control as ctrl
 import matplotlib.pyplot as plt
 
-def getOCP_Value(Nsim, dt, x0, xf, umax):
-    db = DoubleIntegrator(2)
-    db.makeStateSpaceSystem()
-    db.makeDiscreteSystem(dt,'zoh')
+# Written by: AC Rogers, 2016-Oct-14
 
+def getOCP_Value(Nsim, dt, x0, xf, umax):
+'''
+    This function determines the value of Nsim for which the minimum-time,
+    double integrator constraint satisfaction problem is satisfied.
+
+    INPUTS:
+            Nsim : Number of simulation steps
+            dt   : Time step for discretizing the linear system
+            x0   : Initial conditions for maneuver
+            xf   : Terminal conditions for maneuver
+            umax : Maximum control force that can be exerted
+
+    OUTPUT:
+            Nsim : Value of Nsim for which the constraint satisfaction problem
+                   has a valid, optimal solution
+'''
+    # Create an instance of the double integrator
+    db = DoubleIntegrator(2)
+    # Make the state space matrices (we only need A and B)
+    db.makeStateSpaceSystem()
+    # Discretize it
+    db.makeDiscreteSystem(dt,'zoh')
     A = db.sysd.A
     B = db.sysd.B
+
+    # Make the cvxpy environment variables
     x = cvx.Variable(2, Nsim+1)
     u = cvx.Variable(2, Nsim)
+
+    # Initialize an empty array of constraints
     constr = []
+
+    # The cost needs to be zero for minimum time-of-flight
     cost = 0
     # Solve the OCP the first time
     for t in range(Nsim):
+        # Build the problem constraints
         constr += [x[:,t+1] == A*x[:,t] + B*u[:,t],
                    0 <= u[0,t], u[0,t] <= umax,
                    0 <= u[1,t], u[1,t] <= umax]
+
+    # Set the boundary conditions
     constr += [x[0,Nsim] == xf[0], x[1,Nsim] == xf[1], x[0,0] == x0[0], x[1,0] == x0[1]]
+
+    # Create the problem
     prob = cvx.Problem(cvx.Minimize(cost), constr)
+
+    ####### SOLVE THE PROBLEM ON THIS LINE ###############
     prob.solve(verbose = True)
+
+    # Based on the optimality/ infeasibility of the first try, sweep the real
+    # number line (integers only) to find a suitable time-of-flight. If the
+    # first solution was numerically optimal, the sweep decrements Nsim until it
+    # is no longer feasible then adds one as the optimal time-of-flight. If the
+    # problem is initially infeasible, it increments Nsim until a feasible
+    # solution is found, implying that the constraints are satisfied.
     if (prob.status == 'optimal'):
         while (prob.status == 'optimal'):
             Nsim -= 1
@@ -58,17 +97,27 @@ def getOCP_Value(Nsim, dt, x0, xf, umax):
 
 if __name__ == '__main__':
 
+    # Input parameters
+    # Time step
     dt = 0.01
+    # Initial guess for solver
     Nsim_try = 278
+    # Initial conditions
     x0 = [1.0,0.0]
+    # Terminal conditions
     xf = [-1.0,0.0]
+    # Control constraint, u[ii] may never exceed this value
     umax = 1.0
+
+    # Find the optimal value of Nsim to satisfy constraints
     Nsim = getOCP_Value(Nsim_try, dt, x0, xf, umax)
     print "Optimal value for Nsim: " + str(Nsim)
+
+    # Now that we've found the optimal value of Nsim, solve the OCP one more
+    # time with the optimal value, this is the final solution
     db = DoubleIntegrator(2)
     db.makeStateSpaceSystem()
     db.makeDiscreteSystem(dt,'zoh')
-
     A = db.sysd.A
     B = db.sysd.B
     x = cvx.Variable(2, Nsim+1)
@@ -83,8 +132,12 @@ if __name__ == '__main__':
     prob = cvx.Problem(cvx.Minimize(cost), constr)
     prob.solve(verbose = True)
 
+    # Since (for this problem) we had control going strictly positive and
+    # negative, this aggregates the two control signals into one single signal
+    # to plot.
     U = u[0,:].value.A - u[1,:].value.A
 
+    # Plot results.
     f = plt.figure()
     plt.subplot(221)
     plt.plot(x[0,:].value.A.flatten())

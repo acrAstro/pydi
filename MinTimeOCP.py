a6c772a2cc658d4e0b8baa9b1fe47954f5cd30f2
arog <arog3632@vt.edu>
2016-10-14 16:59:54 -0400

    Added a function that automatically line-searches for the minimizing time-of-flight

--- MinTimeOCP.py
+++ MinTimeOCP.py
@@ -4,48 +4,94 @@ import cvxpy as cvx
 import control as ctrl
 import matplotlib.pyplot as plt
 
-#def getOCP_Value(Nsim, boundaryConditions, ctrlConstraint):
-    
+def getOCP_Value(Nsim, dt, x0, xf, umax):
+    db = DoubleIntegrator(2)
+    db.makeStateSpaceSystem()
+    db.makeDiscreteSystem(dt,'zoh')
 
-
-db = DoubleIntegrator(2)
-db.makeStateSpaceSystem()
-db.makeDiscreteSystem(0.01,'zoh')
-
-A = db.sysd.A
-B = db.sysd.B
-
-Nsim = 283
-
-x = cvx.Variable(2, Nsim+1)
-u = cvx.Variable(2, Nsim)
-x0 = [1.0,0.0]
-xf = [-1.0,0.0]
-umax = 1.0
-
-constr = []
-cost = 0
-
-for t in range(Nsim):
-    constr += [x[:,t+1] == A*x[:,t] + B*u[:,t],
+    A = db.sysd.A
+    B = db.sysd.B
+    x = cvx.Variable(2, Nsim+1)
+    u = cvx.Variable(2, Nsim)
+    constr = []
+    cost = 0
+    # Solve the OCP the first time
+    for t in range(Nsim):
+        constr += [x[:,t+1] == A*x[:,t] + B*u[:,t],
                    0 <= u[0,t], u[0,t] <= umax,
                    0 <= u[1,t], u[1,t] <= umax]
-    
-constr += [x[0,Nsim] == xf[0], x[1,Nsim] == xf[1], x[0,0] == x0[0], x[1,0] == x0[1]]
-prob = cvx.Problem(cvx.Minimize(0), constr)
-prob.solve(verbose = True)
+    constr += [x[0,Nsim] == xf[0], x[1,Nsim] == xf[1], x[0,0] == x0[0], x[1,0] == x0[1]]
+    prob = cvx.Problem(cvx.Minimize(cost), constr)
+    prob.solve(verbose = True)
+    if (prob.status == 'optimal'):
+        while (prob.status == 'optimal'):
+            Nsim -= 1
+            x = cvx.Variable(2, Nsim+1)
+            u = cvx.Variable(2, Nsim)
+            constr = []
+            cost = 0
+            print "Trying " + str(Nsim)
+            for t in range(Nsim):
+                constr += [x[:,t+1] == A*x[:,t] + B*u[:,t],
+                           0 <= u[0,t], u[0,t] <= umax,
+                           0 <= u[1,t], u[1,t] <= umax]
+            constr += [x[0,Nsim] == xf[0], x[1,Nsim] == xf[1], x[0,0] == x0[0], x[1,0] == x0[1]]
+            prob = cvx.Problem(cvx.Minimize(cost), constr)
+            prob.solve(verbose = True)
+        return Nsim+1
+    elif (prob.status == 'infeasible'):
+        while (prob.status == 'infeasible'):
+            Nsim += 1
+            x = cvx.Variable(2, Nsim+1)
+            u = cvx.Variable(2, Nsim)
+            constr = []
+            cost = 0
+            print "Trying " + str(Nsim)
+            for t in range(Nsim):
+                constr += [x[:,t+1] == A*x[:,t] + B*u[:,t],
+                           0 <= u[0,t], u[0,t] <= umax,
+                           0 <= u[1,t], u[1,t] <= umax]
+            constr += [x[0,Nsim] == xf[0], x[1,Nsim] == xf[1], x[0,0] == x0[0], x[1,0] == x0[1]]
+            prob = cvx.Problem(cvx.Minimize(cost), constr)
+            prob.solve(verbose = True)
+        return Nsim
 
-print prob.status == 'infeasible'
+if __name__ == '__main__':
 
-U = u[0,:].value.A - u[1,:].value.A
+    dt = 0.01
+    Nsim_try = 278
+    x0 = [1.0,0.0]
+    xf = [-1.0,0.0]
+    umax = 1.0
+    Nsim = getOCP_Value(Nsim_try, dt, x0, xf, umax)
+    print "Optimal value for Nsim: " + str(Nsim)
+    db = DoubleIntegrator(2)
+    db.makeStateSpaceSystem()
+    db.makeDiscreteSystem(dt,'zoh')
 
-f = plt.figure()
-plt.subplot(221)
-plt.plot(x[0,:].value.A.flatten())
-plt.subplot(222)
-plt.plot(x[1,:].value.A.flatten())
-plt.subplot(223)
-plt.plot(U.flatten())
-plt.subplot(224)
-plt.plot(x[0,:].value.A.flatten(), x[1,:].value.A.flatten())
-plt.show()
+    A = db.sysd.A
+    B = db.sysd.B
+    x = cvx.Variable(2, Nsim+1)
+    u = cvx.Variable(2, Nsim)
+    constr = []
+    cost = 0
+    for t in range(Nsim):
+        constr += [x[:,t+1] == A*x[:,t] + B*u[:,t],
+                   0 <= u[0,t], u[0,t] <= umax,
+                   0 <= u[1,t], u[1,t] <= umax]
+    constr += [x[0,Nsim] == xf[0], x[1,Nsim] == xf[1], x[0,0] == x0[0], x[1,0] == x0[1]]
+    prob = cvx.Problem(cvx.Minimize(cost), constr)
+    prob.solve(verbose = True)
+
+    U = u[0,:].value.A - u[1,:].value.A
+
+    f = plt.figure()
+    plt.subplot(221)
+    plt.plot(x[0,:].value.A.flatten())
+    plt.subplot(222)
+    plt.plot(x[1,:].value.A.flatten())
+    plt.subplot(223)
+    plt.plot(U.flatten())
+    plt.subplot(224)
+    plt.plot(x[0,:].value.A.flatten(), x[1,:].value.A.flatten())
+    plt.show()

e7922fc4c349cba7310c08860133257548e7aa36
Andrew <arog3632@vt.edu>
2016-10-14 15:16:44 -0400

    Worked out kinks in min fuel cvxpy example, the problem works now

--- MinTimeOCP.py
+++ MinTimeOCP.py
@@ -18,33 +18,31 @@ x = cvx.Variable(2, Nsim+1)
 u = cvx.Variable(2, Nsim)
 x0 = [1.0,0.0]
 xf = [0.0,0.0]
-print x0[0],x0[1],xf[0],xf[1]
 umax = 1.0
 
 constr = []
 cost = 0
 
 for t in range(Nsim):
-    cost += u[0,t] + u[1,t]
+    cost += cvx.norm(u[:,t],1)
     constr += [x[:,t+1] == A*x[:,t] + B*u[:,t],
                    0 <= u[0,t], u[0,t] <= umax,
                    0 <= u[1,t], u[1,t] <= umax]
     
 constr += [x[0,Nsim] == xf[0], x[1,Nsim] == xf[1], x[0,0] == x0[0], x[1,0] == x0[1]]
 prob = cvx.Problem(cvx.Minimize(cost), constr)
-prob.solve(verbose = True, method = 'dccp')
+prob.solve(verbose = True)
 
-print u.value
-print x.value
+print prob.objective.value
+
+U = u[0,:].value.A - u[1,:].value.A
 
 f = plt.figure()
-plt.subplot(411)
-plt.plot(x[0,:].value)
-plt.subplot(412)
-plt.plot(x[1,:].value)
-plt.subplot(413)
-plt.plot(u[0,:].value)
-plt.subplot(414)
-plt.plot(u[1,:].value)
+plt.subplot(311)
+plt.plot(x[0,:].value.A.flatten())
+plt.subplot(312)
+plt.plot(x[1,:].value.A.flatten())
+plt.subplot(313)
+plt.plot(U.flatten())
 
 plt.show()
